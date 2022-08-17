055d3ef7267b2e19901b0c810c29d8f616f0319d
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-08-17 14:07:43 -0500

    Added departure date form field and functionality to render: trip start date, trip end date & trip lenght in days

--- src/client/js/formSubmit.js
+++ src/client/js/formSubmit.js
@@ -1,5 +1,6 @@
 import { countDownCalculator } from './countDownCalculator'
 import { getGeoInfo } from './getGeoInfo'
+import { tripLength } from './tripLength'
 
 const formSubmit = () => {
     const button = document.getElementById('btn')
@@ -8,11 +9,14 @@ const formSubmit = () => {
 
     let locationValue = document.getElementById('destination').value
     let departureValue = document.getElementById('travel-date').value
+    let returnValue = document.getElementById('return-date').value
 
     console.log(locationValue)
     console.log(departureValue)
+    console.log(returnValue)
     getGeoInfo(locationValue)
     countDownCalculator(departureValue, locationValue)
+    tripLength(departureValue, returnValue)
     })
 }
 
---
+++ src/client/js/tripLength.js
@@ -0,0 +1,26 @@
+const tripLength = (departDate, returnDate) => {
+    const tripRangeEle = document.getElementById('trip-length-header')
+    const departDateNumber = new Date(departDate)
+    const returnDateNumber = new Date(returnDate)
+    const tripDayCount = returnDateNumber.getTime() - departDateNumber.getTime();
+    const tripDayUnits = tripDayCount/(1000 * 3600 * 24)
+    console.log(tripDayUnits);
+    
+    const tripStart = document.getElementById('trip-start')
+    const tripEnd = document.getElementById('trip-end')
+
+    tripRangeEle.innerHTML = `
+        Trip length: ${tripDayUnits} days 
+    `
+
+    tripStart.innerHTML = `
+     departure date: ${departDate}
+    `
+
+    tripEnd.innerHTML = `
+     return date: ${returnDate}
+    `
+
+}
+
+export { tripLength }
