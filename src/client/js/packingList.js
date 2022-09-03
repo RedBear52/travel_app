1ecbffc4cd85417e29782b120f48a51975f72e57
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-09-02 20:54:41 -0500

    Save State

--- src/client/js/packingList.js
+++ src/client/js/packingList.js
@@ -1,5 +1,5 @@
 
-const packingList = (country) => {
+function packingList(country) {
     const packListContainer = document.getElementById('section-container_pack-list')
     const newButton = document.createElement('button')
     packListContainer.appendChild(newButton)
@@ -9,83 +9,89 @@ const packingList = (country) => {
     newButton.addEventListener('click', () => {
         const myModal = document.getElementById('my-modal')
         myModal.style.display = 'block'
-        
+
         const close = document.getElementsByClassName('close')[0]
         close.addEventListener('click', () => {
             myModal.style.display = 'none'
         })
         window.addEventListener('click', (e) => {
             if (e.target == myModal)
-            myModal.style.display = 'none'
+                myModal.style.display = 'none'
         })
     })
 
-        
-        const form = document.getElementById('pack-list-form')
-        form.style.display = 'block'
-        const input = document.getElementById('input')
-        const userUL = document.getElementById('pack-list')
-    console.log(userUL)
-        const userListItems =JSON.parse(localStorage.getItem('listItems'))
-        // not sure about 'userListItems' as the getItem parameter ... 
+    const form = document.getElementById('pack-list-form')
+    form.style.display = 'block'
+    const input = document.getElementById('input')
+    const userUL = document.getElementById('pack-list')
+    const userListItems = JSON.parse(localStorage.getItem('listItems'))
 
-
-        if(userListItems) {
-            userListItems.forEach(listItem => addPackListItem(listItem))
-        }
-
-        form.addEventListener('submit', (e) => {
-            e.preventDefault()
-
-            addPackListItem(input)
+    if (userListItems) {
+        userListItems.forEach(listItem => {
+            console.log(listItem)
+            addPackListItem(listItem)
         })
+    }
 
-        const addPackListItem = (listItem) => {
-            let listItemText = listItem.value
-            if(listItem) {
-                const listEle = document.createElement('li')
-                listEle.setAttribute('id', 'pack-list-item')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
 
-                if(listItem && listItem.completed) {
-                    listEle.classList.add('checked-off')
-                }
+        addPackListItem()
+        // remove 'input' parameter?
+    })
 
-                listEle.innerText = listItemText
-                userUL.appendChild(listEle)
-                input.value = ''
 
-                listEle.addEventListener('dblclick', () => {
-                    listEle.remove()
-                })
 
-                listEle.addEventListener('contextmenu', (e) => {
-                    e.preventDefault()
-                    listEle.classList.toggle('checked-off')
-                })
+    function addPackListItem(listItem) {
+        console.log(listItem)
+        let listItemText = input.value
+        console.log(listItemText)
+        // console.log(listItemText)
+        if (listItem) {
+            listItemText = listItem.text
+            const listEle = document.createElement('li')
+            listEle.setAttribute('id', 'pack-list-item')
+
+            if (listItem && listItem.checkedOff == true) {
+                listEle.classList.add('checked-off')
             }
-        }
 
-        const updateLocalStorage = () => {
-            let listItemsEle = document.querySelectorAll('pack-list')
+            listEle.innerText = listItemText
+            userUL.appendChild(listEle)
+            input.value = ''
 
-            const listItems = []
-
-           listItemsEle.forEach(item => {
-            console.log(item)
-
-                listItems.push({
-                    text: item.innerText,
-                    checkedOff: item.classList.contains('checked-off')
-                })  
-
+            listEle.addEventListener('dblclick', () => {
+                listEle.remove()
+                updateLocalStorage()
             })
 
-            console.log(form)
-            localStorage.setItem('listItems', JSON.stringify(listItems))
-        }  
-
-    updateLocalStorage()
+            listEle.addEventListener('contextmenu', (e) => {
+                e.preventDefault()
+                listEle.classList.toggle('checked-off')
+                updateLocalStorage()
+            })
+        }
+    }
 
 }
+        
+    function updateLocalStorage() {
+    let listItemsEle = document.querySelectorAll('#pack-list-item')
+
+    const listItems = []
+
+    Array.from(listItemsEle).forEach(item => {
+        console.log(item)
+        listItems.push({
+            text: item.innerText,
+            checkedOff: item.classList.contains('checked-off')
+        })
+    })
+
+    console.log(listItems)
+    localStorage.setItem('listItems', JSON.stringify(listItems))
+}  
+
+    // updateLocalStorage()
 
 export { packingList }
