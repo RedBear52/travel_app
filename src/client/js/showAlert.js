3dbaa327d1b20ac278b96b9d45647b4d48c0921e
Ryan Spearman <94654287+RedBear52@users.noreply.github.com>
2022-09-08 12:27:19 -0500

    Organized and Refactored Styling

--- src/client/js/showAlert.js
+++
@@ -1,9 +0,0 @@
-const showAlert = () => {
-    console.log('showalert module activated');
-    // window.addEventListener('load', () => {
-    //     alert(`There was an ERROR: No info for the entered location could be found.
-    //     Please try a new request`)
-    // })
-}
-
-export { showAlert }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './client/styles/base.scss'
-import './client/styles/packingList.scss'
-import './client/styles/main.scss'
-import './client/styles/header.scss'
+import './client/styles/countdown.scss'
+import './client/styles/entryForm.scss'
+import './client/styles/facts.scss'
 import './client/styles/footer.scss'
+import './client/styles/imageArea.scss'
+import './client/styles/header.scss'
+import './client/styles/main.scss'
+import './client/styles/packingList.scss'
+import './client/styles/tripLength.scss'
+import './client/styles/weather.scss'
 
 import { formSubmit } from './client/js/formSubmit'
 
